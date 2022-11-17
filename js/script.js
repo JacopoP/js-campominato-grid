d6035c8badab567946311c8fa7c29e5c92e5e52b
Jac22 <jacopo.prudentino@gmail.com>
2022-11-17 12:06:02 +0100

    aggiunte funzionalità

--- js/script.js
+++ js/script.js
@@ -4,25 +4,33 @@ document.getElementById("generate").addEventListener("click",
     function(){
         container.classList.remove("hide");
         container.innerHTML="";
+        console.clear();
         const n=parseInt(document.getElementById("difficulty").value);
-        for(let i=0; i<n*n; i++){
-            const newElement = createBox();
+        for(let i=1; i<=n*n; i++){
+            const newElement = createBox(i);
             newElement.style.width = 100/n + "%";
             newElement.style.height = 100/n + "%";
-            newElement.innerHTML=i+1;
+            newElement.innerHTML=i;
             container.appendChild(newElement);
         }
     }
 );
 
-function createBox(){
+function createBox(n){
     element=document.createElement("div");
     element.classList="box";
     element.addEventListener("click",
         function(){
-            this.classList.add("selected");
-            console.log(this.innerHTML);
-            this.innerHTML=""
+            this.classList.toggle("selected");
+            if(this.innerHTML!=""){
+                console.log(this.innerHTML);
+            }
+            if(!this.classList.contains("selected")){
+                this.innerHTML=n;
+            }
+            else{
+                this.innerHTML=""
+            }
         }
     );
     return element;
